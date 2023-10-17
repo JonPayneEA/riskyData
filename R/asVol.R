7715ef2c3c4c1eed641c2320fa791c92fe96d61c
Jonathan Payne <jonathan.payne1988@gmail.com>
2023-10-17 11:34:03 +0100

    Updating examples

--- R/asVol.R
+++ R/asVol.R
@@ -10,11 +10,8 @@
 #' # Load bewdley dataset
 #' data(bewdley)
 #'
-#' # Inspect the data
-#' bewdley$data
-#'
-#' # Add a volume column
-#' bewdley$asVol()
+#' # Calculate volumes
+#' asVol(bewdley)
 #'
 #' # Print the bewdley dataset
 #' bewdley
@@ -35,28 +32,22 @@ asVol.data.table <- function(x, area = NULL) {
 
 #' @rdname asVol
 #' @export
-asVol.flowLoad <- function(x, area = NULL) {
-  tDiff <- difftime(x$GaugeData$DateTime[2],
-    x$GaugeData$DateTime[1],
-    units = "mins"
+asVol.HydroImport <- function(x, area = NULL) {
+  tDiff <- difftime(x$data$dateTime[2],
+                    x$data$dateTime[1],
+                    units = "mins"
   )
   tDiff <- as.numeric(tDiff)
-  x$GaugeData[, volume := value * tDiff * 60]
-  return(x)
+  x$data[, volume := value * tDiff * 60]
 }
 
 #' @rdname asVol
 #' @export
-asVol.rainLoad <- function(x, area = NULL) {
-  if (is.null(area)) {
-    stop("Please specify the catchment area")
-  }
-  area <- area * 1000000
-  tDiff <- difftime(x$GaugeData$DateTime[2],
-    x$GaugeData$DateTime[1],
-    units = "mins"
+asVol.HydroAggs <- function(x, area = NULL) {
+  tDiff <- difftime(x$data$dateTime[2],
+                    x$data$dateTime[1],
+                    units = "mins"
   )
   tDiff <- as.numeric(tDiff)
-  x$GaugeData[, volume := (value / 1000) * area]
-  return(x)
+  x$data[, volume := value * tDiff * 60]
 }
--- R/cumSkipNA.R
+++ R/cumSkipNA.R
@@ -10,17 +10,47 @@
 #' @export
 #'
 #' @examples
-#' cumsumNA(rain)
+#' data(chesterton)
+#'
+#' # Apply cumulative sum
+#' dt <- cumsumNA(chesterton)
+#'
+#' plot(dt$dateTime,
+#'      dt$cumSum,
+#'      main = "Cumulative rainfall plot for Chesterton Gauge (WISKI: 164163)",
+#'      xla = "Date Time",
+#'      ylab = "Cumulative rainfall (mm)",
+#'      type = "l")
 cumsumNA <- function(x, ...) {
   UseMethod("cumsumNA", x)
 }
 
 #' @rdname cumsumNA
 #' @export
-cumsumNA <- function(x) {
+cumsumNA.default <- function(x) {
   dt <- data.table(
     dateTime = x$dateTime,
     cumSum = cumsum(ifelse(is.na(x$value), 0, x$value)) + x$value * 0
+  )
+  return(dt)
+}
+
+#' @rdname cumsumNA
+#' @export
+cumsumNA.HydroImport <- function(x) {
+  dt <- data.table(
+    dateTime = x$data$dateTime,
+    cumSum = cumsum(ifelse(is.na(x$data$value), 0, x$data$value)) + x$data$value * 0
+  )
+  return(dt)
+}
+
+#' @rdname cumsumNA
+#' @export
+cumsumNA.HydroAggs <- function(x) {
+  dt <- data.table(
+    dateTime = x$data$dateTime,
+    cumSum = cumsum(ifelse(is.na(x$data$value), 0, x$data$value)) + x$data$value * 0
   )
   return(dt)
 }
