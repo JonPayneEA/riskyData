f1732a3787f555e01bd20f32e688dd7ed8f03be8
Payne <jon.payne@environment-agency.gov.uk>
2024-07-02 08:34:20 +0100

    Adding the loadMulti function

---
+++ R/loadMulti.R
@@ -0,0 +1,66 @@
+#' Load multiple `HydroImport` containers
+#'
+#' @param API Currently only supports the Environment Agency API set as "EA"
+#' @param ID Use to specify a particular WISKI ID or for when downloading all
+#' stations 'wiski' or 'nrfa' to filter results. Additionally to access the real
+#'  time APIs use "flood" for all critical sites and "tidal" for the category A
+#'  tide gauges. Vectors are supported.
+#' @param measure Use this when exporting observations to select the available
+#' parameter. Generally 'flow', 'level', or 'groundwater'
+#' @param period 	This is so you can select the time steps available, generally
+#' 900 (15 min) or 86400 (daily)
+#' @param type Selects the data type, instantaneous, min, max etc.
+#' @param datapoints Allows you to decide where in the time series you are
+#' pulling the data from
+#' @param from First time step of choice
+#' @param to Last time step of choice
+#' @param ...
+#'
+#' @return Multiple `HydroImport` containers
+#' @export
+#'
+#' @examples
+#' loadMulti(ID = c('2001', '2002'),
+#'           measure = c('flow', 'level'),
+#'           period = 900,
+#'           type = 'instantaneous',
+#'           datapoints = 'earliest')
+loadMulti <- function(API = "EA",
+                      ID = NULL,
+                      measure = NULL,
+                      period = NULL,
+                      type = NULL,
+                      datapoints = NULL,
+                      from = NULL,
+                      to = NULL,
+                      ...) {
+
+  if (is.null(ID)) stop("Please include the ID parameter")
+  if (is.null(measure)) stop("Please include the measure parameter")
+  if (is.null(period)) stop("Please include the period parameter")
+  if (is.null(type)) stop("Please include the type parameter")
+
+  if (API == "EA"){
+    dt <- data.table::data.table(ID,
+                                 measure,
+                                 period,
+                                 type,
+                                 datapoints,
+                                 from,
+                                 to)
+    for(i in seq_along(dt$ID)){
+      name <- paste(dt$measure[i], dt$ID[i], sep = "_")
+      print(name)
+      temp <- loadAPI(ID = dt$ID[i],
+                      measure = dt$measure[i],
+                      period = dt$period[i],
+                      type = dt$type[i],
+                      datapoints = dt$datapoints[i],
+                      from = dt$from[i],
+                      to = dt$to[i])
+      assign(print(name), temp, envir = .GlobalEnv)
+    }
+  } else {
+    stope("Currently only the EAs API is supported")
+  }
+}
